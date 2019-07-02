709cffd38b39b66cc0bff5c1d25f3fd858297f05
rohit.s@redbus.com <rohit.s@redbus.com>
2019-07-02 11:57:02 +0530

    conditional operator

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ alert(y === z);
 
 //conjuction &&(AND operator)
 //disjunction ||(OR operator)
+
+var x =2;
+var isEven = true;
+var isEven = (x%2 === 0) ? true: false;
+
+alert(isEven);
+
+//Switch(toggle)
+
+var a =1;
+switch (a) {
+    case 1:
+        alert("Monday");
+        break;
+    case 2:
+        alert("Tuesday");
+        break;
+    default:
+        alert("something else");
+
+}
+
